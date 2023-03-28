992d63f703e2e6d2c22605d589faacc2537bf49c
cgi.net <root@playcat.net>
2023-03-28 15:13:44 +0800

    add: redis集群支持

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 
 ## 特点
 
-1. 支持多种消息队列(Redis stream,Kafka)
+1. 支持多种消息队列(Redis,Kafka)
 2. 延迟消息机制
 3. 易于使用和扩展
 
@@ -12,7 +12,7 @@
 
 Redis单机(**已完成**)
 
-Redis集群(**todo**)
+Redis集群(**已完成**)
 
 Kafka(**todo**)
 
@@ -23,17 +23,30 @@ Kafka(**todo**)
 - PHP >= 7.2
 - Redis >= 5.0
 
-
 ## 安装
 
 本项目基于webman,所以请先安装好webman在执行下面操作。
+
 ```shell
 $ composer require "playcat/queue"
 ```
 
 ## 使用
 
-### 1.创建消费者任务
+### 1.选择合适的消息服务端
+- 使用redis单机(默认)
+  修改config\plugin\playcat\queue\redis.php为自己redis的配置即可
+
+- 使用redis集群机
+  修改config\plugin\playcat\queue\manager.php里的driver为如下内容
+```php
+'driver' => \Playcat\Queue\Driver\Rediscluster::class,
+```
+修改config\plugin\playcat\queue\rediscluster.php为自己redis的配置即可
+
+
+
+### 2.创建消费者任务
 
 #### 编辑'*config/plugin/playcat/queue/redis.php*',修改对应的redis配置
 
@@ -62,7 +75,8 @@ class Test implements Consumer
 
 ```
 
-#### 将'Test.php'保存到'*app/queue/playcat/*'目录下。如果目录不存在就创建它(==可以编辑config/plugin/playcat/queue/process.php中的consumer_dir的地址来改变==)
+#### 将'Test.php'保存到'
+*app/queue/playcat/*'目录下。如果目录不存在就创建它(==可以编辑config/plugin/playcat/queue/process.php中的consumer_dir的地址来改变==)
 
 #### 启动webman的服务
 
@@ -80,7 +94,7 @@ $payload = new Payload();
 $payload->setChannel('test');
 //对应消费队列里的任务使用的数据
 $payload->setQueueData([1,2,3,4]);
-//创建一个立即执行的任务 
+//创建一个立即执行的任务
 Manager::getInstance()->push($payload);
 
 $payload_delay = new Payload();
@@ -88,12 +102,13 @@ $payload_delay = new Payload();
 $payload_delay->setChannel('test');
 //对应消费队列里的任务使用的数据
 $payload_delay->setQueueData([6,7,8,9]);
-//设置60秒后才知晓
+//设置60秒后执行的任务
 $payload_delay->setDelayTime(60);
 Manager::getInstance()->push($payload_delay);`
 ```
 
-### 重试与异常
+### 异常与重试机制
+
 任务在执行过程中未抛出异常则默认执行成功，否则则进入重试阶段.
 重试次数和时间由配置控制，重试间隔时间为当前重试次数的幂函数。
 **Playcat\Queue\Exceptions\DontRetry**异常会忽略掉重试
@@ -106,6 +121,8 @@ Manager::getInstance()->push($payload_delay);`
 - getChannel(): 当前所执行的任务名称
 
 
+- - -
+QQ:318274085
 
 ## License
 
---
+++ src/Driver/Rediscluster.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Playcat\Queue\Driver;
+
+use Playcat\Queue\Exceptions\ParamsError;
+use Playcat\Queue\Model\Payload;
+use Playcat\Queue\Protocols\Driver;
+use RuntimeException;
+
+class Rediscluster extends Base implements Driver
+{
+
+    public const CONSUMERGROUPNAME = 'PLAYCATCONSUMERGROUP';
+    protected $channels = [];
+    private $redis;
+    private $current_id = 0;
+    private $current_channel = 0;
+    private $config;
+
+    public function __construct(string $config_name = 'default')
+    {
+        if (!extension_loaded('redis')) {
+            throw new RuntimeException('Please make sure the PHP Redis extension is installed and enabled.');
+        }
+        $configs = config('plugin.playcat.queue.rediscluster', []);
+        $this->config = $configs[$config_name];
+        $this->getRedis();
+    }
+
+
+    /**
+     * @return void
+     * @throws \RedisClusterException
+     */
+    private function connectRedis(): void
+    {
+        $this->redis = new \RedisCluster(NULL, $this->config['host'], $this->config['options']['timeout'] ?? 1.5, $this->config['options']['timeout'] ?? 1.5, false, $this->config['options']['auth']);
+        $this->redis->setOption(\RedisCluster::OPT_SLAVE_FAILOVER, \RedisCluster::FAILOVER_DISTRIBUTE);
+    }
+
+    /**
+     * @return \RedisCluster
+     */
+    private function getRedis(): \RedisCluster
+    {
+        try {
+            if (!$this->redis || !$this->redis->ping($this->config['host'][0])) {
+                $this->connectRedis();
+            }
+        } catch (\RedisClusterException $e) {
+        }
+        return $this->redis;
+    }
+
+    /***
+     * @param string $channle
+     * @return bool
+     */
+    public function subscribe(string $channle): bool
+    {
+        $this->channels[$channle] = '>';
+        return $this->getRedis()
+            ->xGroup('CREATE', $channle, self::CONSUMERGROUPNAME, '0', true);
+    }
+
+    /**
+     * @return Payload|null
+     * @throws ParamsError
+     */
+    public function shift(): ?Payload
+    {
+        $result = $this->getRedis()
+            ->xReadGroup(self::CONSUMERGROUPNAME, "consumer_" . $this->iconic_id, $this->channels, 1);
+        if ($result) {
+            $this->current_channel = key($result);
+            $this->current_id = key($result[$this->current_channel]);
+            $result = new Payload($result[$this->current_channel][$this->current_id]);
+            $result->setID($this->current_id);
+        } else {
+            $result = null;
+        }
+        return $result;
+    }
+
+    /**
+     * Remove it when done,
+     * @return bool
+     */
+    public function consumerFinished(): bool
+    {
+        return ($this->getRedis()->xAck($this->current_channel, self::CONSUMERGROUPNAME, [$this->current_id])
+            && $this->getRedis()->xDel($this->current_channel, [$this->current_id]));
+    }
+
+    /**
+     * @param Payload $payload
+     * @return string|null  Unique id
+     */
+    public function push(Payload $payload): ?string
+    {
+        return $this->getRedis()->xadd($payload->getChannel(), '*', $payload->getPayload());
+    }
+
+}
